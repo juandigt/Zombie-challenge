557e4e48157e4ac2e02474458fce8410caeb15f4
juan diego garcia <juandigt@hotmail.com>
2018-04-23 17:59:06 +0200

    show endpoint

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::BaseController < ActionController::Base
 
-  rescue_from StandardError,                with: :internal_server_error
+  # rescue_from StandardError,                with: :internal_server_error
   # rescue_from Pundit::NotAuthorizedError,   with: :user_not_authorized
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  # rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   private
 
--- app/controllers/api/v1/zombies_controller.rb
+++ app/controllers/api/v1/zombies_controller.rb
@@ -1,6 +1,21 @@
 class Api::V1::ZombiesController < Api::V1::BaseController
+
+   before_action :set_zombie, only: [ :show ]
+
   def index
     @zombies = Zombie.all
     render json: @zombies
+  end
+
+   def show
+    @weapon = @zombie.weapons
+    @armor = @zombie.armors
+    render json: { zombie: @zombie, zombie_weapons: @weapon, zombie_armors: @armor}
+  end
+
+  private
+
+  def set_zombie
+    @zombie = Zombie.find(params[:id])
   end
 end
