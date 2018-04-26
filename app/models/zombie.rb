d7062cabbf8d84125393609c791fd64c97382ac8
juan diego garcia <juandigt@hotmail.com>
2018-04-26 09:39:59 +0200

    create search filter in index zombie

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,4 +1,5 @@
 class Zombie < ApplicationRecord
+  include Filterable
   has_many :zombie_weapons, dependent: :destroy
   has_many :zombie_armors, dependent: :destroy
 
@@ -7,4 +8,9 @@ class Zombie < ApplicationRecord
 
   # Zombie must have a name
   validates :name, presence: true
+
+  scope :starts_with, -> (name) { where("name like ?", "#{name}%")}
+  scope :speed, -> (speed) { where speed: speed }
+  scope :brains_eaten, -> (brains_eaten) { where brains_eaten: brains_eaten }
+  scope :hit_points, -> (hit_points) { where hit_points: hit_points }
 end
--- app/views/api/v1/zombies/index.json.jbuilder
+++ app/views/api/v1/zombies/index.json.jbuilder
@@ -1,3 +1,11 @@
 json.array! @zombies do |zombie|
   json.extract! zombie, :id, :name, :hit_points, :brains_eaten, :speed, :turn_date
+    json.weapons zombie.weapons do |weapon|
+      json.extract! weapon, :name
+    end
+    json.armors zombie.armors do |armor|
+      json.extract! armor, :name
+    end
 end
+
+
