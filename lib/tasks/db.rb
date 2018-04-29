cc71e78d0149edb0a63b151c4ea91cf59800935b
juan diego garcia <juandigt@hotmail.com>
2018-04-29 19:58:07 +0200

    solving faker problems

---
+++ lib/tasks/db.rb
@@ -0,0 +1,8 @@
+namespace :db do
+
+  task :faker => :environment do
+    require "#{Rails.root}/config/environment.rb"
+    FactoryBot.create_list(:zombie_armor, 30)
+    FactoryBot.create_list(:zombie_weapon, 30)
+  end
+end
